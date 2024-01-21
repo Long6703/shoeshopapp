fd5999ec08ba692fb209ebb51c00c14d5810f366
Anhtrandeptry <AnhTQHE172548@fpt.edu.vn>
2024-01-21 19:13:07 +0700

    reup code

--- src/main/java/Controller/authen/LoginController.java
+++ src/main/java/Controller/authen/LoginController.java
@@ -1,27 +1,55 @@
 package Controller.authen;
 
-import jakarta.servlet.ServletConfig;
+
+import Service.authenService.AccountService;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 
 import java.io.IOException;
-
-public class LoginController extends HttpServlet {
+@WebServlet(name = "LoginController2", urlPatterns = "/login")
+public class LoginController2 extends HttpServlet {
+    private AccountService accountService;
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
+    public void init() throws ServletException {
+        ServletContext servletContext = getServletContext();
+        accountService = (AccountService) servletContext.getAttribute("accountService");
     }
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
+        resp.sendRedirect(req.getContextPath()+ "/view/Login.jsp");
     }
+
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String userName = req.getParameter("account");
+        String password = req.getParameter("password");
+        String loginError = "";
+
+
+        if (userName == null || userName.isEmpty() || password == null || password.isEmpty()) {
+            loginError = "Please enter account and password";
+            req.setAttribute("loginError", loginError);
+            req.getRequestDispatcher("Login.jsp").forward(req, resp);
+            return;
+        }
+        if (accountService.checkLogin(userName,password)) {
+            HttpSession session = req.getSession();
+            session.setAttribute("userName",userName);
+            req.getRequestDispatcher("index.jsp").forward(req, resp);
+        } else {
+            // Invalid credentials, display error message
+            loginError = "Invalid account or password";
+            req.setAttribute("loginError", loginError);
+            req.getRequestDispatcher("Login.jsp").forward(req, resp);
+        }
     }
 }
