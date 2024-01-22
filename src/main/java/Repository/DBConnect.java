949c7437de906d4afa4f2ae06306d0bc83e96689
VuBichNgoc <vubichngoc30082002@gmail.com>
2024-01-22 13:14:02 +0700

    ngoc

---
+++ src/main/java/Repository/DBConnect.java
@@ -0,0 +1,23 @@
+package Repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnect {
+    protected Connection connection;
+    public DBConnect()
+    {
+        try {
+            String user = "sa";
+            String pass = "1";
+            String url = "jdbc:sqlserver://Duy\\SQLEXPRESS:1433;databaseName=SHOE9";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
